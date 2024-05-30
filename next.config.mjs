202594a8bce0f3183df090440d59f291b02247f2
Eirik Reksten <eireksten@gmail.com>
2024-05-30 11:18:19 +0200

    Removed image optimization.

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,12 @@
-import withExportImages from 'next-export-optimize-images';
-
 /**
  * @type {import('next').NextConfig}
  */
-const nextConfig = withExportImages({
+const nextConfig = {
   output: 'export',
   images: {
     deviceSizes: [640, 960, 1080, 1920],
     imageSizes: [64, 128, 256, 384],
   },
-});
+};
 
 export default nextConfig;
