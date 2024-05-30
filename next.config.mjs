3c9a37563164d2fa93377e3ada287a1a3b38c6ab
Eirik Reksten <eireksten@gmail.com>
2024-05-30 11:15:45 +0200

    Added output export to next config

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,7 @@ import withExportImages from 'next-export-optimize-images';
  * @type {import('next').NextConfig}
  */
 const nextConfig = withExportImages({
+  output: 'export',
   images: {
     deviceSizes: [640, 960, 1080, 1920],
     imageSizes: [64, 128, 256, 384],
