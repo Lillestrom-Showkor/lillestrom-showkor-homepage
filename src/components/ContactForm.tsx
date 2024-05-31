6b43dcff50a4cf8f17df9418c98dff66de8609c4
Eirik Reksten <eireksten@gmail.com>
2024-05-31 17:22:59 +0200

    Fixed form validation

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useForm, ValidationError } from '@formspree/react';
 
 import style from 'src/styles/components/ContactForm.module.scss';
@@ -9,17 +9,18 @@ type ContactFormProps = {
 
 export default function ContactForm({ subject }: ContactFormProps) {
   const [state, handleSubmit] = useForm('mzbnlzvy');
+
   return (
     <form className={style.contactform} onSubmit={handleSubmit}>
       <label htmlFor="email">E-postadresse</label>
-      <input id="email" type="email" name="email" />
-      <ValidationError prefix="Email" field="email" errors={state.errors} />
+      <input id="email" type="email" name="email" required={true} />
+      <ValidationError className={style.error} prefix="E-postadresse" field="email" errors={state.errors} />
 
       <input id="subject" type="hidden" name="subject" value={subject || ''} />
 
       <label htmlFor="message">Melding</label>
       <textarea id="message" name="message" />
-      <ValidationError prefix="Message" field="message" errors={state.errors} />
+      <ValidationError className={style.error} prefix="Melding" field="message" errors={state.errors} />
       {!state.succeeded && (
         <button type="submit" disabled={state.submitting}>
           Send
