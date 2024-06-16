7c50f6f754d57c55b68a36ae376d7ddff31edd86
Eirik Reksten <eireksten@gmail.com>
2024-06-16 12:40:58 +0200

    Added introduction to main page.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,11 +13,23 @@ import style from 'src/styles/pages/index.module.scss';
 export default function Index() {
   return (
     <Layout className={style.index} description={info.ogDescription}>
-      <div className={style.collage}>
-        <Image alt={'Lillestrøm på byfesten'} src={coverImg} className={style.coverImg} />
-        <Image alt={'Lillestrøm på byfesten'} src={coverImg2} className={style.coverImg2} />
-        <Image alt={'Lillestrøm på byfesten'} src={coverImg3} className={style.coverImg3} />
-      </div>
+      <h1>Lillestrøm Showkor</h1>
+      <article className={style.description}>
+        <div className={style.collage}>
+          <Image alt={'Lillestrøm på byfesten'} src={coverImg} className={style.coverImg} />
+          <Image alt={'Lillestrøm på byfesten'} src={coverImg2} className={style.coverImg2} />
+          <Image alt={'Lillestrøm på byfesten'} src={coverImg3} className={style.coverImg3} />
+        </div>
+        <p>
+          Lillestrøm Showkor er en dynamisk a cappella korgruppe som ble etablert i januar 2019. Vi er en musikalsk
+          gjeng som elsker å utforske rytmisk musikk innen sjangrene pop, soul, funk og jazz.
+        </p>
+        <p>
+          Basert i hjertet av Lillestrøm, har vi som mål å underholde på ulike eventer og konsertarrangementer. Vår
+          musikalske lekenhet og lidenskap sikrer at hver fremføring blir en uforglemmelig opplevelse. Fra gamle slagere
+          til moderne hits, sørger Lillestrøm Showkor for at det alltid svinger og at publikum har noe å glede seg til.
+        </p>
+      </article>
     </Layout>
   );
 }
