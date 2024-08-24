0a1874a35f99040f38dbfe8d35331dd1f1b67dd6
Eirik Reksten <eireksten@gmail.com>
2024-08-24 11:36:32 +0200

    Added youtube directive.

--- data/posts/2024-06-01-rekruttere-menn.md
+++ data/posts/2024-06-01-rekruttere-menn.md
@@ -10,3 +10,5 @@ Er du mann og har lurt på om å synge i kor hadde vært noe for deg? Da er det 
 Vi trenger flere tenorer og basser. Vi har også plass til beatboxing i en del av sangene våre, så ikke nøl med å ta kontakt om det er noe du liker.
 
 Det er kjempegøy på korøvelse. Her får du masse hjelp til å utvikle deg som sanger, artist og å finne på mye moro sammen med en fantastisk gruppe sangere. Ta en kikk på vår side [Bli med i koret](/join) og meld interesse, da vel!
+
+::youtube[Det regner menn]{#HMlc5ZVJsUY}
--- src/lib/markdown.ts
+++ src/lib/markdown.ts
@@ -1,11 +1,13 @@
 import remarkParse from 'remark-parse';
 import remarkFrontmatter from 'remark-frontmatter';
+import remarkDirective from 'remark-directive';
 import remarkRehype from 'remark-rehype';
 import rehypeMinify from 'rehype-preset-minify';
 import rehypeStringify from 'rehype-stringify';
-import rehypeSanitize from 'rehype-sanitize';
+import rehypeSanitize, { defaultSchema } from 'rehype-sanitize';
 import { unified } from 'unified';
 import { matter } from 'vfile-matter';
+import {visit} from 'unist-util-visit'
 
 import PostType from 'src/interfaces/post';
 
@@ -14,20 +16,28 @@ export default async function parseMarkdown(slug: string, markdown: string): Pro
   const file = await unified()
     .use(remarkParse)
     .use(remarkFrontmatter)
-    .use(function() {
-      return function(tree, file) {
-        const p = (tree.children as Array<any>).find((value) => value.type === 'paragraph');
-        if (p != null) {
-          const text = p.children.find((value) => value.type === 'text');
-          if (text !== null) {
-            file.data.excerpt = text.value;
-          }
-        }
-        matter(file);
+    .use(remarkExcerpt)
+    .use(remarkMatter)
+    .use(remarkDirective)
+    .use(directiveYoutube)
+    .use(remarkRehype)
+    .use(rehypeSanitize, {
+      tagNames: [
+        ...defaultSchema.tagNames,
+        'iframe'
+      ],
+      attributes: {
+        ...defaultSchema.attributes,
+        iframe: [
+          'src',
+          'width',
+          'height',
+          'frameBorder',
+          'allow',
+          'allowFullScreen'
+        ],
       }
     })
-    .use(remarkRehype)
-    .use(rehypeSanitize)
     .use(rehypeMinify)
     .use(rehypeStringify)
     .process(markdown);
@@ -43,4 +53,71 @@ export default async function parseMarkdown(slug: string, markdown: string): Pro
     slug: slug,
   };
 
+}
+
+function remarkExcerpt() {
+  return function(tree, file) {
+    const p = (tree.children as Array<any>).find((value) => value.type === 'paragraph');
+    if (p != null) {
+      const text = p.children.find((value) => value.type === 'text');
+      if (text !== null) {
+        file.data.excerpt = text.value;
+      }
+    }
+  }
+}
+
+function remarkMatter() {
+  return function(tree, file) {
+    matter(file);
+  }
+}
+
+
+function directiveYoutube() {
+  /**
+   * @param {import('mdast').Root} tree
+   *   Tree.
+   * @param {import('vfile').VFile} file
+   *   File.
+   * @returns {undefined}
+   *   Nothing.
+   */
+  return (tree, file) => {
+    visit(tree, function (node) {
+      if (
+        node.type === 'containerDirective' ||
+        node.type === 'leafDirective' ||
+        node.type === 'textDirective'
+      ) {
+
+        if (node.name !== 'youtube') return;
+
+        const data = node.data || (node.data = {});
+        const attributes = node.attributes || {};
+        const id = attributes.id;
+
+        if (node.type === 'textDirective') {
+          file.fail(
+            'Unexpected `:youtube` text directive, use two colons for a leaf directive',
+            node
+          );
+        }
+
+        if (!id) {
+          file.fail('Unexpected missing `id` on `youtube` directive', node);
+        }
+
+        data.hName = 'iframe';
+        data.hProperties = {
+          src: 'https://www.youtube.com/embed/' + id,
+          width: 560,
+          height: 315,
+          frameBorder: 0,
+          allow: 'accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share',
+          allowFullScreen: true
+        };
+      }
+    })
+  }
 }
